f6f4f2f95430e32b2d5f5b46f257b9df013a8a90
Mathias Claeys <mathiasclaeys@hotmail.com>
2014-12-19 12:19:13 +0100

    update for git change

--- python/app/dialog.py
+++ python/app/dialog.py
@@ -18,46 +18,85 @@ import threading
 from sgtk.platform.qt import QtCore, QtGui
 from .ui.dialog import Ui_Dialog
 
+wtd_pipeline = sgtk.platform.import_framework("tk-framework-wtd", "pipeline")
+# wtd_maya = sgtk.platform.import_framework("tk-framework-wtd", "maya")
+
 def show_dialog(app_instance):
-    """
-    Shows the main dialog window.
-    """
-    # in order to handle UIs seamlessly, each toolkit engine has methods for launching
-    # different types of windows. By using these methods, your windows will be correctly
-    # decorated and handled in a consistent fashion by the system. 
-    
-    # we pass the dialog class to this method and leave the actual construction
-    # to be carried out by toolkit.
-    app_instance.engine.show_dialog("Starter Template App...", app_instance, AppDialog)
-    
+	"""
+	Shows the main dialog window.
+	"""
+	# in order to handle UIs seamlessly, each toolkit engine has methods for launching
+	# different types of windows. By using these methods, your windows will be correctly
+	# decorated and handled in a consistent fashion by the system. 
+	
+	# we pass the dialog class to this method and leave the actual construction
+	# to be carried out by toolkit.
+	app_instance.engine.show_dialog("Shotloader...", app_instance, AppDialog)
+	
 
 
 class AppDialog(QtGui.QWidget):
-    """
-    Main application dialog window
-    """
-    
-    def __init__(self):
-        """
-        Constructor
-        """
-        # first, call the base class and let it do its thing.
-        QtGui.QWidget.__init__(self)
-        
-        # now load in the UI that was created in the UI designer
-        self.ui = Ui_Dialog() 
-        self.ui.setupUi(self)
-        
-        # most of the useful accessors are available through the Application class instance
-        # it is often handy to keep a reference to this. You can get it via the following method:
-        self._app = sgtk.platform.current_bundle()
-        
-        # via the self._app handle we can for example access:
-        # - The engine, via self._app.engine
-        # - A Shotgun API instance, via self._app.shotgun
-        # - A tk API instance, via self._app.tk 
-        
-        # lastly, set up our very basic UI
-        self.ui.context.setText("Current Context: %s" % self._app.context)
-        
-        
+	"""
+	Main application dialog window
+	"""
+	
+	def __init__(self):
+		"""
+		Constructor
+		"""
+		# first, call the base class and let it do its thing.
+		QtGui.QWidget.__init__(self)
+		
+		# now load in the UI that was created in the UI designer
+		self.ui = Ui_Dialog() 
+		self.ui.setupUi(self)
+		
+		# most of the useful accessors are available through the Application class instance
+		# it is often handy to keep a reference to this. You can get it via the following method:
+		self._app = sgtk.platform.current_bundle()
+		
+		# via the self._app handle we can for example access:
+		# - The engine, via self._app.engine
+		# - A Shotgun API instance, via self._app.shotgun
+		# - A tk API instance, via self._app.tk 
+		
+		# lastly, set up our very basic UI
+		print dir(self._app.context.sgtk.shotgun)
+		print self._app.context.project
+		# self.ui.context.setText("Current Context: %s" % self._app.context)
+		
+		self.shotlist = self.loadShots()
+		# print self.shotlist
+		
+	def loadValues(self):
+		print "Todo: load positionlist"
+		self.poslist = None
+		self.currenctShot = str(self.ui.context.getText())
+		
+		if self.currenctShot != "" and self.currenctShot in self.shotlist:
+			print "go to next step with shot %s!" %self.currenctShot
+		else:
+			print "No existing shot found in shotgun"
+			return None
+			
+		self.poslist = wtd_pipeline.Positionlist()
+
+	def createCheckboxes(self):
+		print "Todo: create checkboxes"
+		
+	def loadShots(self):
+		current_project = self._app.context.project
+		fields = ['id', 'code']
+		filters = [
+			['project','is',current_project]
+			]
+		shots = self._app.context.sgtk.shotgun.find("Shot",filters,fields)
+		adjusted_shotlist = {}
+		for s in shots:
+			adjusted_shotlist[s["code"].replace("_","-").replace("q","").replace("s","")] = s
+		return adjusted_shotlist
+		
+	def checkShotExistance(self):
+		print "Todo: check if shot exists"
+		
+	
--- python/app/ui/dialog.py
+++ python/app/ui/dialog.py
@@ -9,31 +9,71 @@
 from tank.platform.qt import QtCore, QtGui
 
 class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(431, 392)
-        self.horizontalLayout = QtGui.QHBoxLayout(Dialog)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.logo_example = QtGui.QLabel(Dialog)
-        self.logo_example.setText("")
-        self.logo_example.setPixmap(QtGui.QPixmap(":/res/sg_logo.png"))
-        self.logo_example.setObjectName("logo_example")
-        self.horizontalLayout.addWidget(self.logo_example)
-        self.context = QtGui.QLabel(Dialog)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.context.sizePolicy().hasHeightForWidth())
-        self.context.setSizePolicy(sizePolicy)
-        self.context.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
-        self.context.setObjectName("context")
-        self.horizontalLayout.addWidget(self.context)
+	def setupUi(self, Dialog):
+		Dialog.setObjectName("Dialog")
+		Dialog.resize(331, 291)
+		Dialog.setLayoutDirection(QtCore.Qt.LeftToRight)
+		self.verticalLayoutWidget_2 = QtGui.QWidget(Dialog)
+		self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(10, 60, 311, 181))
+		self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
+		self.layout_checkboxes = QtGui.QVBoxLayout(self.verticalLayoutWidget_2)
+		self.layout_checkboxes.setContentsMargins(6, -1, -1, -1)
+		self.layout_checkboxes.setObjectName("layout_checkboxes")
+		self.checkBox_specimen = QtGui.QCheckBox(self.verticalLayoutWidget_2)
+		self.checkBox_specimen.setObjectName("checkBox_specimen")
+		self.layout_checkboxes.addWidget(self.checkBox_specimen)
+		self.horizontalLayoutWidget = QtGui.QWidget(Dialog)
+		self.horizontalLayoutWidget.setGeometry(QtCore.QRect(10, 10, 311, 31))
+		self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+		self.horizontalLayout = QtGui.QHBoxLayout(self.horizontalLayoutWidget)
+		self.horizontalLayout.setSizeConstraint(QtGui.QLayout.SetFixedSize)
+		self.horizontalLayout.setObjectName("horizontalLayout")
+		self.shot_label = QtGui.QLabel(self.horizontalLayoutWidget)
+		font = QtGui.QFont()
+		font.setPointSize(10)
+		self.shot_label.setFont(font)
+		self.shot_label.setObjectName("shot_label")
+		self.horizontalLayout.addWidget(self.shot_label)
+		spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+		self.horizontalLayout.addItem(spacerItem)
+		self.shotname = QtGui.QLineEdit(self.horizontalLayoutWidget)
+		self.shotname.setMinimumSize(QtCore.QSize(150, 0))
+		self.shotname.setMaximumSize(QtCore.QSize(150, 16777215))
+		self.shotname.setStyleSheet("border-width: 1px; \n"
+			"background-image: url(:/res/search.png);\n"
+			"background-repeat: no-repeat;\n"
+			"background-position: center left;\n"
+			"border-style: inset; \n"
+			"border-color: #535353; \n"
+			"border-radius: 9px; \n"
+			"padding-left: 15px")
+		self.shotname.setObjectName("shotname")
+		self.horizontalLayout.addWidget(self.shotname)
+		self.lbl_extraInfo = QtGui.QLabel(self.horizontalLayoutWidget)
+		font = QtGui.QFont()
+		font.setPointSize(7)
+		self.lbl_extraInfo.setFont(font)
+		self.lbl_extraInfo.setLayoutDirection(QtCore.Qt.LeftToRight)
+		self.lbl_extraInfo.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+		self.lbl_extraInfo.setObjectName("lbl_extraInfo")
+		self.horizontalLayout.addWidget(self.lbl_extraInfo)
+		self.Line = QtGui.QFrame(Dialog)
+		self.Line.setGeometry(QtCore.QRect(10, 40, 420, 16))
+		self.Line.setFrameShape(QtGui.QFrame.HLine)
+		self.Line.setFrameShadow(QtGui.QFrame.Sunken)
+		self.Line.setObjectName("Line")
+		self.build_btn = QtGui.QPushButton(Dialog)
+		self.build_btn.setGeometry(QtCore.QRect(10, 250, 420, 31))
+		self.build_btn.setObjectName("build_btn")
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+		self.retranslateUi(Dialog)
+		QtCore.QMetaObject.connectSlotsByName(Dialog)
 
-    def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "The Current Sgtk Environment", None, QtGui.QApplication.UnicodeUTF8))
-        self.context.setText(QtGui.QApplication.translate("Dialog", "Your Current Context: ", None, QtGui.QApplication.UnicodeUTF8))
+	def retranslateUi(self, Dialog):
+		Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "The Current Sgtk Environment", None, QtGui.QApplication.UnicodeUTF8))
+		self.checkBox_specimen.setText(QtGui.QApplication.translate("Dialog", "Select all", None, QtGui.QApplication.UnicodeUTF8))
+		self.shot_label.setText(QtGui.QApplication.translate("Dialog", "<html><head/><body><p><span style=\" font-size:large;\">Load shot</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+		self.lbl_extraInfo.setText(QtGui.QApplication.translate("Dialog", "(001-001)", None, QtGui.QApplication.UnicodeUTF8))
+		self.build_btn.setText(QtGui.QApplication.translate("Dialog", "Build shot", None, QtGui.QApplication.UnicodeUTF8))
 
 from . import resources_rc
